1e4c9cc4413e5698533c540abcf274e853f8996f
victorbrugnolo <brugnolovictor@gmail.com>
2024-05-21 21:57:36 -0300

    import base project

---
+++ internal/infra/graph/resolver.go
@@ -0,0 +1,11 @@
+package graph
+
+import "github.com/victorbrugnolo/golang-clean-arch-challenge/internal/usecase"
+
+// This file will not be regenerated automatically.
+//
+// It serves as dependency injection for your app, add any dependencies you require here.
+
+type Resolver struct {
+	CreateOrderUseCase usecase.CreateOrderUseCase
+}
